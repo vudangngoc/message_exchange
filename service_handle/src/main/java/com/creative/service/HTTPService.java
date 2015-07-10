59745b41399e2dff68c8930cc032df6f0c2bd176
vudangngoc <vudangngoc@gmail.com>
2015-07-10 22:16:32 +0700

    add demo page

--- service_handle/src/main/java/com/creative/service/HTTPService.java
+++ service_handle/src/main/java/com/creative/service/HTTPService.java
@@ -11,12 +11,40 @@ public class HTTPService {
 	public HTTPService(){
 		super();
 		this.builder = new StringBuilder();
-		builder.append("<h1>HELLO WORLD</h1>")
-				.append("");
+		builder.append("<html>")
+				.append("<head>")
+				.append("<script type=\"text/javascript\">")
+				.append("function sendMessage(){")
+				.append("request = '%7B';")
+				.append("request += 'COMMAND:' + document.getElementById(\"command\").value + ',';")
+				.append("request += 'FROM:' + document.getElementById(\"from\").value + ',';")
+				.append("request += 'TO:' + document.getElementById(\"to\").value + ',';")
+				.append("request += 'DATA:' + document.getElementById(\"data\").value;")
+				.append("request += '%7D';")
+				.append("var xmlHttp = new XMLHttpRequest();")
+				.append("xmlHttp.open(\"GET\", request, true );")
+				.append("xmlHttp.send();")
+				.append("document.getElementById(\"result\").value = xmlHttp.responseText;")
+				.append("}")
+				.append("</script>")
+				.append("</head>")
+				.append("<body>")
+				.append("<div>")
+				.append("<div style=\"height:auto;width:30%;\">From:</div><div style=\"height:auto;width:700%;\"><input type=\"text\" id=\"from\"></div>")
+				.append("<div style=\"height:auto;width:30%;\">To:</div><div style=\"height:auto;width:700%;\"><input type=\"text\" id=\"to\"></div>")
+				.append("<div style=\"height:auto;width:30%;\">Command:</div><div style=\"height:auto;width:700%;\"><input type=\"text\" id=\"command\"></div>")
+				.append("<div style=\"height:auto;width:30%;\">Data:</div><div style=\"height:auto;width:700%;\"><input type=\"text\" id=\"data\"></div>")
+				.append("</div>")
+				.append("<input type=\"submit\" onclick=\"sendMessage()\" value=\"Submit\">")
+				.append("<div id=\"result\"></div>")
+				.append("</body>")
+				.append("</html>");
+		
 	}
 
 	public void onMessage(PrintStream printStream, String path) {
-		printStream.println(builder.toString());
+		if(path.startsWith("/demo"))
+			printStream.println(builder.toString());
 		printStream.close();
 	}
 
