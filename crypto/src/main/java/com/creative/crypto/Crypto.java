65fbe90e4d8f5d7391fe5e7db631d8e71b791142
Vu Dang Ngoc <ngocvd@exoplatform.com>
2015-07-27 12:34:20 +0700

    init crypto module

--- crypto/src/main/java/com/creative/crypto/Crypto.java
+++
@@ -1,6 +0,0 @@
-package com.creative.crypto;
-
-public class Crypto {
-	public static String enCrypt(String data){return data;}
-	public static String deCrypt(String data){return data;}
-}
---
+++ crypto/src/test/java/com/creative/cryto/test/TestStringXORer.java
@@ -0,0 +1,24 @@
+package com.creative.cryto.test;
+
+import java.util.UUID;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.creative.crypto.StringXORer;
+
+public class TestStringXORer {
+	@Test
+	public void testEncrypt(){
+		StringXORer test = new StringXORer();
+		UUID uuid = UUID.randomUUID();
+		String content = "{COMMAND:STATE_GET,FROM:esp7_1@demo,TO:nil,DATA:NA}";
+		String key = uuid.toString();
+		String encryptContent = test.encode(content, key);
+		System.out.println("encrypt data: "+ encryptContent);
+		System.out.println("encrypt key: "+ key);
+		Assert.assertEquals(content, test.decode(encryptContent,key));
+		
+	}
+}
