ea824d782af3eaec924fb05bf1b62f1bf14351a7
NgocVD <ngocvd@exoplatform.com>
2015-11-23 15:50:10 +0700

    init unit test for TCPConnector

--- front-server/src/main/java/com/creative/connector/back_server/TCPConnector.java
+++ front-server/src/main/java/com/creative/connector/back_server/TCPConnector.java
@@ -3,20 +3,43 @@
  */
 package com.creative.connector.back_server;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * @author ngocvd
  *
  */
 public class TCPConnector implements Connector {
 
+  private Socket socket;
+  private static final Logger logger = LoggerFactory.getLogger(TCPConnector.class);
   /**
    * Setup TCP server properties
    * @param address
    * @param port
    */
-  public void setUp(String address, String port) {
-    // TODO Auto-generated method stub
-
+  public void setUp(String host, int port) {
+    InetAddress address;
+    try {
+      address = InetAddress.getByName(host);
+      socket = new Socket(address, port);
+    } catch (UnknownHostException e) {
+      logger.debug("Cannot resolve host " + host);
+    } catch (IOException e) {
+      logger.debug("Cannot create socket");
+    }
   }
 
   /* (non-Javadoc)
@@ -24,8 +47,45 @@ public class TCPConnector implements Connector {
    */
   @Override
   public String sendMessage(String message) {
-    // TODO Auto-generated method stub
-    return null;
+    try
+    {
+      if(socket == null) return "";
+      if(!message.endsWith("\n")) message += "\n";
+      //Send the message to the server
+      OutputStream os = socket.getOutputStream();
+      OutputStreamWriter osw = new OutputStreamWriter(os);
+      BufferedWriter bw = new BufferedWriter(osw);
+
+      bw.write(message);
+      bw.flush();
+      System.out.println("Message sent to the server");
+
+      //Get the return message from the server
+      InputStream is = socket.getInputStream();
+      InputStreamReader isr = new InputStreamReader(is);
+      BufferedReader br = new BufferedReader(isr);
+      String result = br.readLine();
+      
+      logger.debug("Message get from the server",result);
+      return result;
+    }
+    catch (Exception exception)
+    {
+      exception.printStackTrace();
+    }
+    finally
+    {
+      //Closing the socket
+      try
+      {
+        socket.close();
+      }
+      catch(Exception e)
+      {
+        logger.debug("Closing socket faile ",e);
+      }
+    }
+    return "";
   }
 
 }
--- front-server/src/test/java/com/creative/connector/back_server/TestTCPConnector.java
+++ front-server/src/test/java/com/creative/connector/back_server/TestTCPConnector.java
@@ -1,5 +1,85 @@
 package com.creative.connector.back_server;
 
-public class TestTCPConnector {
+import static org.junit.Assert.assertEquals;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+import org.junit.Test;
+
+public class TestTCPConnector {
+  @Test
+  public void testSend(){
+    //Given
+    MockTCPServer server = new MockTCPServer(25000);
+    Thread t = new Thread(server);
+    t.start();
+    TCPConnector connector = new TCPConnector();
+    connector.setUp("127.0.0.1", 25000);
+    //When
+    try {
+      Thread.sleep(500);
+    } catch (InterruptedException e) {
+      // TODO Auto-generated catch block
+      e.printStackTrace();
+    }
+    String result = connector.sendMessage("Hi there!");
+    //Then
+    assertEquals("Hi there!",result);
+    server.setRunning(false);
+  }
+  private static class MockTCPServer implements Runnable{
+    private Socket socket;
+    private int port;
+    private boolean isRunning = true;
+    public void setRunning(boolean state){
+      this.isRunning = state;
+    }
+    public MockTCPServer(int port){
+      this.port = port;
+    }
+    public void run(){
+      try
+      {
+        if(port == 0)
+          port = 25000;
+        ServerSocket serverSocket = new ServerSocket(port);
+        //Server is running always. This is done using this while(true) loop
+        while(isRunning)
+        {
+          //Reading the message from the client
+          socket = serverSocket.accept();
+          InputStream is = socket.getInputStream();
+          InputStreamReader isr = new InputStreamReader(is);
+          BufferedReader br = new BufferedReader(isr);
+          String message = br.readLine();
+          //Sending the response back to the client.
+          OutputStream os = socket.getOutputStream();
+          OutputStreamWriter osw = new OutputStreamWriter(os);
+          BufferedWriter bw = new BufferedWriter(osw);
+          bw.write(message + "\n");
+          bw.flush();
+        }
+      }
+      catch (Exception e)
+      {
+        e.printStackTrace();
+      }
+      finally
+      {
+        try
+        {
+          socket.close();
+        }
+        catch(Exception e){}
+      }
+    }
+  }
 }
+
