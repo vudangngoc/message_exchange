5145aacdf40ff63f7fa949c1443d6d3be02f485a
NgocVD <ngocvd@exoplatform.com>
2015-12-02 11:52:08 +0700

    reduce max memory usage, just for demo perpose

--- tcp_server/start_server.sh
+++ tcp_server/start_server.sh
@@ -1,1 +1,1 @@
-cd target/ && java -Xmx256M -server -classpath tcp_server-0.0.1.jar:lib/crypto-0.0.1.jar:lib/json-20140107.jar:lib/disruptor-3.3.2.jar:lib/log4j-1.2.17.jar  com.creative.server.TCPServer 10001 1000 512
+cd target/ && java -Xmx64M -server -classpath tcp_server-0.0.1.jar:lib/crypto-0.0.1.jar:lib/json-20140107.jar:lib/disruptor-3.3.2.jar:lib/log4j-1.2.17.jar  com.creative.server.TCPServer 10001 1000 512
