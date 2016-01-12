4e51b658d9f86b30a7fed172ab3046bd03b714a8
vudangngoc <vudangngoc@gmail.com>
2016-01-12 23:33:55 +0700

    debugging OrderLinkedList

--- libs/src/main/java/com/creative/OrderLinkedList.java
+++ libs/src/main/java/com/creative/OrderLinkedList.java
@@ -12,7 +12,7 @@ public class OrderLinkedList<T extends Comparable<T>> {
   public OrderLinkedList(){
     shortcut = new ArrayList<OrderLinkedList<T>.Item>();
     this.size = 0;
-    logger.setLevel(Level.INFO);
+    logger.setLevel(Level.DEBUG);
   }
   final static Logger logger = Logger.getLogger(OrderLinkedList.class);
   public static int PARTITION_SIZE = 50;
